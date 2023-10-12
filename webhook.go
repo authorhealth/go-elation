b68e9108c37be312191f36a3638ddb06c32e9f03
Frank Terragna <fterragna@gmail.com>
2023-10-12 11:45:52 -0400

    Return Event when verifying the webhook (#8)

--- webhook.go
+++ webhook.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"crypto/ed25519"
 	"encoding/base64"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"io"
@@ -16,27 +17,41 @@ const (
 
 var ErrPublicKeyLength = errors.New("incorrect length of public key")
 
-func VerifyWebhook(r *http.Request, publicKey []byte) error {
+type Event struct {
+	Data          json.RawMessage `json:"data"`
+	Action        string          `json:"action"`
+	EventID       int64           `json:"event_id"`
+	ApplicationID string          `json:"application_id"`
+	Resource      string          `json:"resource"`
+}
+
+func VerifyWebhook(r *http.Request, publicKey []byte) (*Event, error) {
 	if len(publicKey) != ed25519.PublicKeySize {
-		return ErrPublicKeyLength
+		return nil, ErrPublicKeyLength
 	}
 
 	sig, err := base64.StdEncoding.DecodeString(r.Header.Get(WebhookSignatureHeader))
 	if err != nil {
-		return fmt.Errorf("decoding Ed25519 signature: %w", err)
+		return nil, fmt.Errorf("decoding Ed25519 signature: %w", err)
 	}
 
 	body, err := io.ReadAll(r.Body)
 	if err != nil {
-		return fmt.Errorf("reading request body: %w", err)
+		return nil, fmt.Errorf("reading request body: %w", err)
 	}
 	//nolint
 	_ = r.Body.Close()
 	r.Body = io.NopCloser(bytes.NewBuffer(body))
 
 	if !ed25519.Verify(publicKey, body, sig) {
-		return errors.New("verifying signature")
+		return nil, errors.New("verifying signature")
 	}
 
-	return nil
+	event := &Event{}
+	err = json.Unmarshal(body, event)
+	if err != nil {
+		return nil, fmt.Errorf("unmarshaling body: %w", err)
+	}
+
+	return event, nil
 }
