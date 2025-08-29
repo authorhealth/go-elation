e558d5844bedac1bd3fd86782eb1be7094d841c1
Ellie Hastings <ellie.hastings@authorhealth.com>
2025-08-29 10:31:41 -0400

    Revert "Add Find and Get Bills (#129)" (#130)
    This reverts commit 8a3dd4e41420328a2e2ec0481908f0f522672043.

--- bill_test.go
+++ bill_test.go
@@ -6,9 +6,7 @@ import (
 	"io"
 	"net/http"
 	"net/http/httptest"
-	"strconv"
 	"testing"
-	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -24,7 +22,7 @@ func TestBillService_Create(t *testing.T) {
 				Patient:         64901939201,
 				Practice:        65540,
 				Physician:       64811630594,
-				CPTs:            []*BillCPTCreate{},
+				CPTs:            []*BillCPT{},
 			},
 		},
 		"all specified fields request": {
@@ -34,7 +32,7 @@ func TestBillService_Create(t *testing.T) {
 				Patient:         64901939201,
 				Practice:        65540,
 				Physician:       64811630594,
-				CPTs: []*BillCPTCreate{
+				CPTs: []*BillCPT{
 					{
 						CPT:        "12",
 						Units:      "1.0",
@@ -113,7 +111,7 @@ func TestBillService_Create_already_exists(t *testing.T) {
 		Patient:         64901939201,
 		Practice:        65540,
 		Physician:       64811630594,
-		CPTs:            []*BillCPTCreate{},
+		CPTs:            []*BillCPT{},
 	}
 
 	srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -153,115 +151,4 @@ func TestBillService_Create_already_exists(t *testing.T) {
 	assert.Nil(created)
 	assert.NotNil(res)
 	assert.ErrorIs(err, ErrBillExist)
-}
-
-func TestBillService_Find(t *testing.T) {
-	assert := assert.New(t)
-
-	opts := &FindBillsOptions{
-		Pagination: &Pagination{
-			Limit:  1,
-			Offset: 2,
-		},
-
-		AssignedPhysician: []int64{1},
-		BillID:            []int64{2},
-		FromServiceDate:   time.Date(2023, 5, 15, 0, 0, 0, 0, time.UTC),
-		ToServiceDate:     time.Date(2023, 5, 20, 0, 0, 0, 0, time.UTC),
-		Patient:           []int64{3},
-		Practice:          []int64{4},
-		SigningPhysician:  []int64{5},
-		VisitNoteID:       []int64{6},
-	}
-
-	srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if tokenRequest(w, r) {
-			return
-		}
-
-		assert.Equal(http.MethodGet, r.Method)
-		assert.Equal("/bills", r.URL.Path)
-
-		assignedPhysician := r.URL.Query().Get("assigned_physician")
-		billID := r.URL.Query().Get("bill_id")
-		fromServiceDate := r.URL.Query().Get("from_service_date")
-		toServiceDate := r.URL.Query().Get("to_service_date")
-		patient := r.URL.Query().Get("patient")
-		practice := r.URL.Query().Get("practice")
-		signingPhysician := r.URL.Query().Get("signing_physician")
-		visitNoteID := r.URL.Query().Get("visit_note_id")
-
-		limit := r.URL.Query().Get("limit")
-		offset := r.URL.Query().Get("offset")
-
-		assert.Equal(opts.AssignedPhysician, sliceStrToInt64([]string{assignedPhysician}))
-		assert.Equal(opts.BillID, sliceStrToInt64([]string{billID}))
-		assert.Equal(opts.FromServiceDate.Format(time.RFC3339), fromServiceDate)
-		assert.Equal(opts.ToServiceDate.Format(time.RFC3339), toServiceDate)
-		assert.Equal(opts.Patient, sliceStrToInt64([]string{patient}))
-		assert.Equal(opts.Practice, sliceStrToInt64([]string{practice}))
-		assert.Equal(opts.SigningPhysician, sliceStrToInt64([]string{signingPhysician}))
-		assert.Equal(opts.VisitNoteID, sliceStrToInt64([]string{visitNoteID}))
-
-		assert.Equal(opts.Limit, strToInt(limit))
-		assert.Equal(opts.Offset, strToInt(offset))
-
-		b, err := json.Marshal(Response[[]*Bill]{
-			Results: []*Bill{
-				{
-					ID: 1,
-				},
-				{
-					ID: 2,
-				},
-			},
-		})
-		assert.NoError(err)
-
-		w.Header().Set("Content-Type", "application/json")
-		//nolint
-		w.Write(b)
-	}))
-	defer srv.Close()
-
-	client := NewHTTPClient(srv.Client(), srv.URL+"/token", "", "", srv.URL)
-	svc := BillService{client}
-
-	found, res, err := svc.Find(context.Background(), opts)
-	assert.NotNil(found)
-	assert.NotNil(res)
-	assert.NoError(err)
-}
-
-func TestBillService_Get(t *testing.T) {
-	assert := assert.New(t)
-
-	var id int64 = 1
-
-	srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if tokenRequest(w, r) {
-			return
-		}
-
-		assert.Equal(http.MethodGet, r.Method)
-		assert.Equal("/bills/"+strconv.FormatInt(id, 10), r.URL.Path)
-
-		b, err := json.Marshal(&Patient{
-			ID: id,
-		})
-		assert.NoError(err)
-
-		w.Header().Set("Content-Type", "application/json")
-		//nolint
-		w.Write(b)
-	}))
-	defer srv.Close()
-
-	client := NewHTTPClient(srv.Client(), srv.URL+"/token", "", "", srv.URL)
-	svc := BillService{client}
-
-	found, res, err := svc.Get(context.Background(), id)
-	assert.NotNil(found)
-	assert.NotNil(res)
-	assert.NoError(err)
 }
