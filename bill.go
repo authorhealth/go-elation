d12f011cdedd2f82c6162da21a942e0fd3c8d7db
Ellie Hastings <ellie.hastings@authorhealth.com>
2026-01-16 14:54:28 -0500

    Add Get and Find methods to Bill Service (#153)

--- bill.go
+++ bill.go
@@ -18,7 +18,9 @@ const billExistError = "The visit note provided already has a bill associated wi
 var ErrBillExist = errors.New("bill already exists for visit note")
 
 type BillServicer interface {
-	Create(ctx context.Context, create *BillCreate) (*Bill, *http.Response, error)
+	Create(ctx context.Context, create *BillCreate) (*CreatedBill, *http.Response, error)
+	Find(ctx context.Context, opts *FindBillOptions) (*Response[[]*Bill], *http.Response, error)
+	Get(ctx context.Context, id int64) (*Bill, *http.Response, error)
 }
 
 var _ BillServicer = (*BillService)(nil)
@@ -27,65 +29,65 @@ type BillService struct {
 	client *HTTPClient
 }
 
-type Bill struct {
-	ID                   int64         `json:"id"`                      //: 65099661468,
-	RefNumber            *string       `json:"ref_number"`              //: null,                        // string(50). required for PATCH that marks bill as processed.
-	ServiceDate          time.Time     `json:"service_date"`            //: "2016-10-12T12:00:00Z",
-	BillingDate          *time.Time    `json:"billing_date"`            //: null,                        // datetime(iso8601). required for PATCH that marks bill as processed.
-	BillingStatus        string        `json:"billing_status"`          //: "Unbilled",
-	BillingError         *string       `json:"billing_error"`           //: null,                        // string(200). required for PATCH that marks bill as failed.
-	BillingRawError      *string       `json:"billing_raw_error"`       //: null,                        // longtext. optional for PATCH that marks bill as failed.
-	Notes                string        `json:"notes"`                   //: "patient has not paid yet",
-	CPTs                 []*BillCPT    `json:"cpts"`                    //: [{}],
-	Payment              int64         `json:"payment"`                 //: 142502884606313,
-	VisitNote            int64         `json:"visit_note"`              //: 64409108504,
-	VisitNoteSignedDate  time.Time     `json:"visit_note_signed_date"`  //: "2016-10-12T22:11:01Z",
-	VisitNoteDeletedDate *time.Time    `json:"visit_note_deleted_date"` //: null,
-	ReferringProvider    *BillProvider `json:"referring_provider"`      //: {},
-	BillingProvider      *int64        `json:"billing_provider"`        //: 42120898,
-	RenderingProvider    *int64        `json:"rendering_provider"`      //: 68382673,
-	SupervisingProvider  *int64        `json:"supervising_provider"`    //: 52893234,
-	OrderingProvider     *BillProvider `json:"ordering_provider"`       //: {}
-	ServiceLocation      int64         `json:"service_location"`        //: 141103949480183,
-	Physician            int64         `json:"physician"`               //: 64811630594,
-	Practice             int64         `json:"practice"`                //: 65540,
-	Patient              int64         `json:"patient"`                 //: 64901939201,
-	PriorAuthorization   *string       `json:"prior_authorization"`     //: "1234-ABC",
-	Metadata             any           `json:"metadata"`                //: null,
-	CreatedDate          time.Time     `json:"created_date"`            //: "2016-05-23T17:50:50Z",
-	LastModifiedDate     time.Time     `json:"last_modified_date"`      //: "2016-10-12T22:39:46Z"
+type CreatedBill struct {
+	ID                   int64             `json:"id"`                      //: 65099661468,
+	RefNumber            *string           `json:"ref_number"`              //: null,                        // string(50). required for PATCH that marks bill as processed.
+	ServiceDate          time.Time         `json:"service_date"`            //: "2016-10-12T12:00:00Z",
+	BillingDate          *time.Time        `json:"billing_date"`            //: null,                        // datetime(iso8601). required for PATCH that marks bill as processed.
+	BillingStatus        string            `json:"billing_status"`          //: "Unbilled",
+	BillingError         *string           `json:"billing_error"`           //: null,                        // string(200). required for PATCH that marks bill as failed.
+	BillingRawError      *string           `json:"billing_raw_error"`       //: null,                        // longtext. optional for PATCH that marks bill as failed.
+	Notes                string            `json:"notes"`                   //: "patient has not paid yet",
+	CPTs                 []*CreatedBillCPT `json:"cpts"`                    //: [{}],
+	Payment              int64             `json:"payment"`                 //: 142502884606313,
+	VisitNote            int64             `json:"visit_note"`              //: 64409108504,
+	VisitNoteSignedDate  time.Time         `json:"visit_note_signed_date"`  //: "2016-10-12T22:11:01Z",
+	VisitNoteDeletedDate *time.Time        `json:"visit_note_deleted_date"` //: null,
+	ReferringProvider    *BillProvider     `json:"referring_provider"`      //: {},
+	BillingProvider      *int64            `json:"billing_provider"`        //: 42120898,
+	RenderingProvider    *int64            `json:"rendering_provider"`      //: 68382673,
+	SupervisingProvider  *int64            `json:"supervising_provider"`    //: 52893234,
+	OrderingProvider     *BillProvider     `json:"ordering_provider"`       //: {}
+	ServiceLocation      int64             `json:"service_location"`        //: 141103949480183,
+	Physician            int64             `json:"physician"`               //: 64811630594,
+	Practice             int64             `json:"practice"`                //: 65540,
+	Patient              int64             `json:"patient"`                 //: 64901939201,
+	PriorAuthorization   *string           `json:"prior_authorization"`     //: "1234-ABC",
+	Metadata             any               `json:"metadata"`                //: null,
+	CreatedDate          time.Time         `json:"created_date"`            //: "2016-05-23T17:50:50Z",
+	LastModifiedDate     time.Time         `json:"last_modified_date"`      //: "2016-10-12T22:39:46Z"
 }
 
 type BillCreate struct {
-	ServiceLocation     int64         `json:"service_location"`               //: 10           // required
-	VisitNote           int64         `json:"visit_note"`                     //: 64409108504, // required
-	Patient             int64         `json:"patient"`                        //: 64901939201, // required
-	Practice            int64         `json:"practice"`                       //: 65540, 		   // required
-	Physician           int64         `json:"physician"`                      //: 64811630594, // required
-	CPTs                []*BillCPT    `json:"cpts"`                           //: [{}],        // required
-	BillingProvider     int64         `json:"billing_provider,omitempty"`     //: 42120898,
-	RenderingProvider   int64         `json:"rendering_provider,omitempty"`   //: 68382673,
-	SupervisingProvider int64         `json:"supervising_provider,omitempty"` //: 52893234,
-	ReferringProvider   *BillProvider `json:"referring_provider,omitempty"`   //: {},
-	OrderingProvider    *BillProvider `json:"ordering_provider,omitempty"`    //: {},
-	PriorAuthorization  string        `json:"prior_authorization,omitempty"`  //: "1234-ABC",
-	PaymentAmount       float64       `json:"payment_amount,omitempty"`       //: 10.00,
-	Notes               string        `json:"notes,omitempty"`                //: "patient has not paid yet",
+	ServiceLocation     int64             `json:"service_location"`               //: 10           // required
+	VisitNote           int64             `json:"visit_note"`                     //: 64409108504, // required
+	Patient             int64             `json:"patient"`                        //: 64901939201, // required
+	Practice            int64             `json:"practice"`                       //: 65540, 		   // required
+	Physician           int64             `json:"physician"`                      //: 64811630594, // required
+	CPTs                []*CreatedBillCPT `json:"cpts"`                           //: [{}],        // required
+	BillingProvider     int64             `json:"billing_provider,omitempty"`     //: 42120898,
+	RenderingProvider   int64             `json:"rendering_provider,omitempty"`   //: 68382673,
+	SupervisingProvider int64             `json:"supervising_provider,omitempty"` //: 52893234,
+	ReferringProvider   *BillProvider     `json:"referring_provider,omitempty"`   //: {},
+	OrderingProvider    *BillProvider     `json:"ordering_provider,omitempty"`    //: {},
+	PriorAuthorization  string            `json:"prior_authorization,omitempty"`  //: "1234-ABC",
+	PaymentAmount       float64           `json:"payment_amount,omitempty"`       //: 10.00,
+	Notes               string            `json:"notes,omitempty"`                //: "patient has not paid yet",
 }
 
-type BillDX struct {
+type CreatedBillDX struct {
 	ICD10Code string `json:"icd10_code"`
 }
-type BillCPT struct {
-	CPT        string   `json:"cpt"`                 //: "99213",
-	Modifiers  []string `json:"modifiers,omitempty"` //: ["10"],
-	DXs        []BillDX `json:"dxs"`                 //: ["D23.4"],
-	AltDXs     []string `json:"alt_dxs,omitempty"`   //: ["216.4"],
-	UnitCharge string   `json:"unit_charge"`         //: "10.0",
-	Units      string   `json:"units"`               //: "1.0"
+type CreatedBillCPT struct {
+	CPT        string          `json:"cpt"`                 //: "99213",
+	Modifiers  []string        `json:"modifiers,omitempty"` //: ["10"],
+	DXs        []CreatedBillDX `json:"dxs"`                 //: ["D23.4"],
+	AltDXs     []string        `json:"alt_dxs,omitempty"`   //: ["216.4"],
+	UnitCharge string          `json:"unit_charge"`         //: "10.0",
+	Units      string          `json:"units"`               //: "1.0"
 }
 
-type BillPayment struct {
+type CreatedBillPayment struct {
 	Amount        string    `json:"amount"`         //: "10.00",
 	WhenCollected time.Time `json:"when_collected"` //: "2016-10-12T22:11:01Z"
 }
@@ -112,11 +114,11 @@ type BillServiceLocation struct {
 	DeletedDate        *time.Time `json:"deleted_date"`          //: null
 }
 
-func (b *BillService) Create(ctx context.Context, create *BillCreate) (*Bill, *http.Response, error) {
+func (b *BillService) Create(ctx context.Context, create *BillCreate) (*CreatedBill, *http.Response, error) {
 	ctx, span := b.client.tracer.Start(ctx, "create bill", trace.WithSpanKind(trace.SpanKindClient))
 	defer span.End()
 
-	bill := &Bill{}
+	bill := &CreatedBill{}
 
 	res, err := b.client.request(ctx, http.MethodPost, "/bills", nil, create, &bill)
 	if err != nil {
@@ -140,4 +142,93 @@ func (b *BillService) Create(ctx context.Context, create *BillCreate) (*Bill, *h
 	}
 
 	return bill, res, nil
+}
+
+type BillCPT struct {
+	CPT        string   `json:"cpt"`                 //: "99213"
+	Modifiers  []string `json:"modifiers,omitempty"` //: ["10"]
+	DXs        []string `json:"dxs"`                 //: ["D23.4"]
+	AltDXs     []string `json:"alt_dxs,omitempty"`   //: ["216.4"]
+	UnitCharge string   `json:"unit_charge"`         //: "10.0"
+	Units      string   `json:"units"`               //: "1.0"
+}
+
+type BillPayment struct {
+	Amount        string    `json:"amount"`         //: "10.00"
+	WhenCollected time.Time `json:"when_collected"` //: "2016-10-12T22:11:01Z"
+}
+
+type Bill struct {
+	ID                   int64           `json:"id"`                      //: 65099661468
+	RefNumber            *string         `json:"ref_number"`              //: null                        // string(50). required for PATCH that marks bill as processed.
+	ServiceDate          time.Time       `json:"service_date"`            //: "2016-10-12T12:00:00Z"
+	BillingDate          *time.Time      `json:"billing_date"`            //: null                        // datetime(iso8601). required for PATCH that marks bill as processed.
+	BillingStatus        string          `json:"billing_status"`          //: "Unbilled"
+	BillingError         *string         `json:"billing_error"`           //: null                        // string(200). required for PATCH that marks bill as failed.
+	BillingRawError      *string         `json:"billing_raw_error"`       //: null                        // longtext. optional for PATCH that marks bill as failed.
+	Notes                string          `json:"notes"`                   //: "patient has not paid yet"
+	CPTs                 []*BillCPT      `json:"cpts"`                    //: [{}]
+	Payment              BillPayment     `json:"payment"`                 //: {}
+	VisitNote            int64           `json:"visit_note"`              //: 64409108504
+	VisitNoteSignedDate  time.Time       `json:"visit_note_signed_date"`  //: "2016-10-12T22:11:01Z"
+	VisitNoteDeletedDate *time.Time      `json:"visit_note_deleted_date"` //: null
+	ReferringProvider    *BillProvider   `json:"referring_provider"`      //: {}
+	BillingProvider      *int64          `json:"billing_provider"`        //: 42120898
+	RenderingProvider    *int64          `json:"rendering_provider"`      //: 68382673
+	SupervisingProvider  *int64          `json:"supervising_provider"`    //: 52893234
+	OrderingProvider     *BillProvider   `json:"ordering_provider"`       //: {}
+	ServiceLocation      ServiceLocation `json:"service_location"`        //: {}
+	Physician            int64           `json:"physician"`               //: 64811630594
+	Practice             int64           `json:"practice"`                //: 65540
+	Patient              int64           `json:"patient"`                 //: 64901939201
+	PriorAuthorization   *string         `json:"prior_authorization"`     //: "1234-ABC"
+	Metadata             any             `json:"metadata"`                //: null
+	CreatedDate          time.Time       `json:"created_date"`            //: "2016-05-23T17:50:50Z"
+	LastModifiedDate     time.Time       `json:"last_modified_date"`      //: "2016-10-12T22:39:46Z"
+}
+
+func (b *BillService) Get(ctx context.Context, id int64) (*Bill, *http.Response, error) {
+	ctx, span := b.client.tracer.Start(ctx, "get bill", trace.WithSpanKind(trace.SpanKindClient))
+	defer span.End()
+
+	bill := &Bill{}
+
+	res, err := b.client.request(ctx, http.MethodGet, fmt.Sprintf("/bills/%d", id), nil, nil, &bill)
+	if err != nil {
+		span.RecordError(err)
+		span.SetStatus(codes.Error, "error making request")
+
+		return nil, res, fmt.Errorf("making request: %w", err)
+	}
+
+	return bill, res, nil
+}
+
+type FindBillOptions struct {
+	*Pagination
+
+	AssignedPhysician []int64   `url:"assigned_physician,omitempty"` //: [64811630594]
+	BillID            []int64   `url:"bill_id,omitempty"`            //: [143945720594544]
+	FromServiceDate   time.Time `url:"from_service_date,omitempty"`  //: "2016-05-23T17:50:50Z"
+	ToServiceDate     time.Time `url:"to_service_date,omitempty"`    //: "2016-05-23T17:50:50Z"
+	Patient           []int64   `url:"patient,omitempty"`            //: [141289139994622]
+	SigningPhysician  int64     `url:"signing_physician,omitempty"`  //: 64811630594
+	VisitNoteID       []int64   `url:"visit_note_id,omitempty"`      //: 12314823928
+}
+
+func (b *BillService) Find(ctx context.Context, opts *FindBillOptions) (*Response[[]*Bill], *http.Response, error) {
+	ctx, span := b.client.tracer.Start(ctx, "find bills", trace.WithSpanKind(trace.SpanKindClient))
+	defer span.End()
+
+	out := &Response[[]*Bill]{}
+
+	res, err := b.client.request(ctx, http.MethodGet, "/bills", opts, nil, &out)
+	if err != nil {
+		span.RecordError(err)
+		span.SetStatus(codes.Error, "error making request")
+
+		return nil, res, fmt.Errorf("making request: %w", err)
+	}
+
+	return out, res, nil
 }
--- bill_test.go
+++ bill_test.go
@@ -6,7 +6,9 @@ import (
 	"io"
 	"net/http"
 	"net/http/httptest"
+	"strconv"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -22,7 +24,7 @@ func TestBillService_Create(t *testing.T) {
 				Patient:         64901939201,
 				Practice:        65540,
 				Physician:       64811630594,
-				CPTs:            []*BillCPT{},
+				CPTs:            []*CreatedBillCPT{},
 			},
 		},
 		"all specified fields request": {
@@ -32,13 +34,13 @@ func TestBillService_Create(t *testing.T) {
 				Patient:         64901939201,
 				Practice:        65540,
 				Physician:       64811630594,
-				CPTs: []*BillCPT{
+				CPTs: []*CreatedBillCPT{
 					{
 						CPT:        "12",
 						Units:      "1.0",
 						UnitCharge: "122.0",
 						Modifiers:  []string{"modifier 1", "modifier 2"},
-						DXs:        []BillDX{{ICD10Code: "dx 1"}, {ICD10Code: "dx 2"}},
+						DXs:        []CreatedBillDX{{ICD10Code: "dx 1"}, {ICD10Code: "dx 2"}},
 						AltDXs:     []string{"alt dx 1", "alt dx 2"},
 					},
 				},
@@ -82,7 +84,7 @@ func TestBillService_Create(t *testing.T) {
 
 				assert.Equal(testCase.create, create)
 
-				b, err := json.Marshal(&Bill{})
+				b, err := json.Marshal(&CreatedBill{})
 				assert.NoError(err)
 
 				w.Header().Set("Content-Type", "application/json")
@@ -111,7 +113,7 @@ func TestBillService_Create_already_exists(t *testing.T) {
 		Patient:         64901939201,
 		Practice:        65540,
 		Physician:       64811630594,
-		CPTs:            []*BillCPT{},
+		CPTs:            []*CreatedBillCPT{},
 	}
 
 	srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -151,4 +153,99 @@ func TestBillService_Create_already_exists(t *testing.T) {
 	assert.Nil(created)
 	assert.NotNil(res)
 	assert.ErrorIs(err, ErrBillExist)
+}
+
+func TestBillService_Get(t *testing.T) {
+	assert := assert.New(t)
+
+	var id int64 = 65099661468
+
+	srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if tokenRequest(w, r) {
+			return
+		}
+
+		assert.Equal(http.MethodGet, r.Method)
+		assert.Equal("/bills/"+strconv.FormatInt(id, 10), r.URL.Path)
+
+		b, err := json.Marshal(&Bill{
+			ID: id,
+		})
+		assert.NoError(err)
+
+		w.Header().Set("Content-Type", "application/json")
+		//nolint
+		w.Write(b)
+	}))
+	defer srv.Close()
+
+	client := NewHTTPClient(srv.Client(), srv.URL+"/token", "", "", srv.URL)
+	svc := BillService{client}
+
+	found, res, err := svc.Get(context.Background(), id)
+	assert.NotNil(found)
+	assert.NotNil(res)
+	assert.NoError(err)
+	assert.Equal(id, found.ID)
+}
+
+func TestBillService_Find(t *testing.T) {
+	assert := assert.New(t)
+
+	fromServiceDate := time.Date(2026, 1, 1, 0, 0, 0, 0, time.UTC)
+	toServiceDate := time.Date(2026, 1, 31, 0, 0, 0, 0, time.UTC)
+
+	opts := &FindBillOptions{
+		Patient:           []int64{141289139994622},
+		BillID:            []int64{143945720594544},
+		FromServiceDate:   fromServiceDate,
+		ToServiceDate:     toServiceDate,
+		AssignedPhysician: []int64{64811630594},
+		SigningPhysician:  64811630594,
+		VisitNoteID:       []int64{12314823928},
+	}
+
+	bills := []*Bill{
+		{ID: 101},
+		{ID: 102},
+	}
+
+	srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if tokenRequest(w, r) {
+			return
+		}
+
+		assert.Equal(http.MethodGet, r.Method)
+		assert.Equal("/bills", r.URL.Path)
+
+		q := r.URL.Query()
+		assert.Equal("141289139994622", q.Get("patient"))
+		assert.Equal("143945720594544", q.Get("bill_id"))
+		assert.Equal(fromServiceDate.Format(time.RFC3339), q.Get("from_service_date"))
+		assert.Equal(toServiceDate.Format(time.RFC3339), q.Get("to_service_date"))
+		assert.Equal("64811630594", q.Get("assigned_physician"))
+		assert.Equal("64811630594", q.Get("signing_physician"))
+		assert.Equal("12314823928", q.Get("visit_note_id"))
+
+		b, err := json.Marshal(Response[[]*Bill]{
+			Results: bills,
+		})
+		assert.NoError(err)
+
+		w.Header().Set("Content-Type", "application/json")
+		//nolint
+		w.Write(b)
+	}))
+	defer srv.Close()
+
+	client := NewHTTPClient(srv.Client(), srv.URL+"/token", "", "", srv.URL)
+	svc := BillService{client}
+
+	billsRes, res, err := svc.Find(context.Background(), opts)
+	assert.NotNil(billsRes)
+	assert.NotNil(res)
+	assert.NoError(err)
+
+	assert.Len(billsRes.Results, 2)
+	assert.Equal(bills, billsRes.Results)
 }
