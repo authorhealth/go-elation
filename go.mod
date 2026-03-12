f0cc2f7dff4fa1d7d65743c3a8124ff9dc5b04c0
Ellie Hastings <ellie.hastings@authorhealth.com>
2026-03-12 13:14:13 -0400

    Upgrade Go to 1.26 (#162)

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/authorhealth/go-elation
 
-go 1.24.0
+go 1.26.0
 
 require (
 	go.opentelemetry.io/otel v1.41.0
--- ptr.go
+++ ptr.go
@@ -1,5 +1,7 @@
 package elation
 
+// Deprecated: use new() instead of Ptr.
+// Will be removed soon after upgrading to Go version 1.26
 func Ptr[T any](val T) *T {
 	return &val
 }
